3e96d2a4d5370f56b9548e80e02304ae47520f09
leonardo emil <theflaco56@hotmail.com>
2019-06-21 17:27:51 -0400

    tipo personas test

---
+++ Tarea6Tests4/BLL/TipoPersonasTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarea6.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.BLL.Tests
+{
+    [TestClass()]
+    public class TipoPersonasTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<TipoPersonas> db = new RepositorioBase<TipoPersonas>();
+
+            TipoPersonas tpersona = new TipoPersonas()
+            {
+                TipoPersonaId = 1,
+                PersonaId = 1,
+                Nombre = "prueba"
+            };
+
+            Assert.IsTrue(db.Guardar(tpersona));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<TipoPersonas> db = new RepositorioBase<TipoPersonas>();
+
+            TipoPersonas tipoPersonas = new TipoPersonas()
+            {
+                TipoPersonaId = 1,
+                PersonaId = 1,
+                Nombre = "p1"
+            };
+
+            Assert.IsTrue(db.Modificar(tipoPersonas));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<TipoPersonas> db = new RepositorioBase<TipoPersonas>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<TipoPersonas> db = new RepositorioBase<TipoPersonas>();
+
+            Assert.IsNotNull(db.Buscar(2));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<TipoComprobantes> db = new RepositorioBase<TipoComprobantes>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+    }
+}
