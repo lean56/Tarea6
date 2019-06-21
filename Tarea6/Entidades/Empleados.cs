cca9ddc6b4b3af8313b192293a5681525af73c70
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:35:08 -0400

    Creando entidad empleados

---
+++ Tarea6/Entidades/Empleados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Empleados
+    {
+        [Key]
+        public int EmpleadoId { get; set; }
+        public int EstadoId { get; set; }
+        public int CargoId { get; set; }
+        public int PersonaId { get; set; }
+
+        public Empleados()
+        {
+            EmpleadoId = 0;
+            EstadoId = 0;
+            CargoId = 0;
+            PersonaId = 0;
+        }
+    }
+}
