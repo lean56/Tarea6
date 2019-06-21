6952dc3189e84fc3528e482fcd592a9898c90069
leonardo emil <theflaco56@hotmail.com>
2019-06-21 15:55:45 -0400

    modelo tests

--- Tarea6/DAL/Contexto.cs
+++ Tarea6/DAL/Contexto.cs
@@ -10,22 +10,28 @@ namespace Tarea6.DAL
 {
     public class Contexto : DbContext
     {
-        public DbSet<Personas>personas {get;set;}
-        public DbSet<TipoPersonas> TipoPersona { get; set; }
-        public DbSet<Clientes> Cliente { get; set; }
         public DbSet<Cargos> Cargo { get; set; }
         public DbSet<Categorias> Categoria { get; set; }
-        public DbSet<Empleados> empleado { get; set; }
+        public DbSet<Clientes> Cliente { get; set; }
         public DbSet<Compras> Compra { get; set; }
-
-        public DbSet<Estados>estado { get; set; }
-
+        public DbSet<DetalleCompras>detalleCompra { get; set; }
+       // public DbSet<DetalleVenta> detalleVenta { get; set; }
+        public DbSet<Empleados> empleado { get; set; }
+        public DbSet<Estados> estado { get; set; }
         public DbSet<Imagenes> imagene { get; set; }
-
-        public DbSet<Marcas>marca { get; set; }
-        public DbSet<Modelos>modelo { get; set; }
-
-
+        public DbSet<Marcas> marca { get; set; }
+        public DbSet<Modelos> modelo { get; set; }
+        public DbSet<Permisos> permiso { get; set; }
+        public DbSet<Personas>personas {get;set;}
+        public DbSet<Productos>producto { get; set; }
+        public DbSet<Proveedores>proveedore { get; set; }
+        public DbSet<TipoComprobantes>tipoComprobante { get; set; }
+        public DbSet<TipoPersonas> TipoPersona { get; set; }
+        public DbSet<UnidadMedidas>unidadMedida { get; set; }
+        public DbSet<Usuarios> usuario { get; set; }
+        public DbSet<Usuarios_Has_Permisos> Usuarios_Has_Permiso { get; set; }
+        public DbSet<Ventas> venta { get; set; }
+       
         public Contexto() : base("Constr") { }
         
         public void FixEfProviderServicesProblem()
---
+++ Tarea6Tests4/BLL/ModelosTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarea6.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.BLL.Tests
+{
+    [TestClass()]
+    public class ModelosTests
+    {
+        
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Modelos> db = new RepositorioBase<Modelos>();
+
+            Modelos modelos = new Modelos()
+            {
+                ModeloId = 2,
+                NombreModelo="p2"
+            };
+
+            Assert.IsTrue(db.Guardar(modelos));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Modelos> db = new RepositorioBase<Modelos>();
+
+            Modelos modelos = new Modelos()
+            {
+                ModeloId = 1,
+                NombreModelo = "prueba"
+            };
+
+            Assert.IsTrue(db.Modificar(modelos));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Modelos> db = new RepositorioBase<Modelos>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Modelos> db = new RepositorioBase<Modelos>();
+
+            Assert.IsNotNull(db.Buscar(2));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Modelos> db = new RepositorioBase<Modelos>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+
+    }
+}
