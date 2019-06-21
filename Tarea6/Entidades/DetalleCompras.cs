4d1c564e53e868651775429e13b5a43014681114
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:15:05 -0400

    Creando entidad detalle compras

---
+++ Tarea6/Entidades/DetalleCompras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class DetalleCompras
+    {
+        [Key]
+        public int DetalleCompraId { get; set; }
+        public int CompraId { get; set; }
+        public int ProductoId { get; set; }
+        public double Unidades { get; set; }
+        public double CostoUnidad { get; set; }
+        public double Total { get; set; }
+
+        public DetalleCompras()
+        {
+            DetalleCompraId = 0;
+            CompraId = 0;
+            ProductoId = 0;
+            Unidades = 0;
+            CostoUnidad = 0;
+            Total = 0;
+        }
+    }
+}
