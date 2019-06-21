25d463f07fd8fd5827418ae6acaadb2cc98f345b
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:23:03 -0400

    creando entidad Modelos

---
+++ Tarea6/Entidades/Modelos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Modelos
+    {
+        [Key]
+        public int ModeloId { get; set; }
+        public string  NombreModelo { get; set; }
+
+        public Modelos()
+        {
+            ModeloId = 0;
+            NombreModelo = string.Empty;
+        }
+    }
+}
