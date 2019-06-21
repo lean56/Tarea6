0c15bcfcadc29e3001e33f21a65069da38b3892b
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:18:15 -0400

    creando entidad marcas

---
+++ Tarea6/Entidades/Marcas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Marcas
+    {
+        [Key]
+        public int MarcasId { get; set; }
+        public string NombreMarca { get; set; }
+
+        public Marcas()
+        {
+            MarcasId = 0;
+            NombreMarca = string.Empty;
+        }
+    }
+}
