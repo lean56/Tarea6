fc7ae89645c3c9f3c73e9b33c2adbd583949ee3c
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:38:55 -0400

    creando entidad estados

---
+++ Tarea6/Entidades/Estados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Estados
+    {
+        [Key]
+        public int EstadoId { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string Estado { get; set; }
+    }
+}
