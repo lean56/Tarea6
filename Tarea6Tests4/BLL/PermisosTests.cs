4b9e6d6243afeac4065e4cd9e35ef638621f4136
leonardo emil <theflaco56@hotmail.com>
2019-06-21 16:12:18 -0400

    permisos tests

---
+++ Tarea6Tests4/BLL/PermisosTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarea6.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.BLL.Tests
+{
+    [TestClass()]
+    public class PermisosTests
+    {
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Permisos> db = new RepositorioBase<Permisos>();
+
+            Permisos permisos = new Permisos()
+            {
+                PermisoId = 1,
+                Descripcion = "prueba",
+                Funcionalidad="ff"
+            };
+
+            Assert.IsTrue(db.Guardar(permisos));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Permisos> db = new RepositorioBase<Permisos>();
+
+            Permisos permisos = new Permisos()
+            {
+                PermisoId = 2,
+                Descripcion = "p2",
+                Funcionalidad = "ff"
+            };
+
+            Assert.IsTrue(db.Modificar(permisos));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Permisos> db = new RepositorioBase<Permisos>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Permisos> db = new RepositorioBase<Permisos>();
+
+            Assert.IsNotNull(db.Buscar(2));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Permisos> db = new RepositorioBase<Permisos>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+
+    }
+}
