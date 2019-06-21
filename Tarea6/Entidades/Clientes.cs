953d75eaf146f729da94c95a3f406e800f77cd63
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:27:17 -0400

    creando entidad Clientes

---
+++ Tarea6/Entidades/Clientes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public int PersonaId { get; set; }
+        public int EstadoId { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            PersonaId = 0;
+            EstadoId = 0;
+            FechaCreacion = DateTime.Now;
+        }
+    }
+}
