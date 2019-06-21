536881e62bbc962a126bf5b3ba6115038b088f75
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:55:18 -0400

    creando entidad Permisos

---
+++ Tarea6/Entidades/Permisos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Permisos
+    {
+        [Key]
+        public int PermisoId { get; set; }
+        public string Descripcion { get; set; }
+        public string Funcionalidad { get; set; }
+
+        public Permisos()
+        {
+            PermisoId = 0;
+            Descripcion = string.Empty;
+            Funcionalidad = string.Empty;
+        }
+    }
+}
