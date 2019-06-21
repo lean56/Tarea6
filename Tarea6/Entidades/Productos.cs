6c0a585dbac5fb95cc614d9a2b225e54738226bf
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:12:32 -0400

    creando entidad productos

---
+++ Tarea6/Entidades/Productos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaFabricacion { get; set; }
+        public double CostoCompra { get; set; }
+        public double Stock { get; set; }
+        public int UnidadMedidaId { get; set; }
+        public int ImagenId { get; set; }
+        public int CategoriaId { get; set; }
+        public int MarcaId { get; set; }
+        public int ModeloId { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Nombre = string.Empty;
+            Descripcion = string.Empty;
+            FechaFabricacion = DateTime.Now;
+            CostoCompra = 0;
+            Stock = 0;
+            UnidadMedidaId = 0;
+            ImagenId = 0;
+            CategoriaId = 0;
+            MarcaId = 0;
+            ModeloId = 0;
+        }
+    }
+}
