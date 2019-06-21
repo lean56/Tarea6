b49d1a55b444a06196b30e6af0b6da40fe9ad226
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:07:08 -0400

    creando entidad Compras

---
+++ Tarea6/Entidades/Compras.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Compras
+    {
+        [Key]
+        public int CompraId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public int ProveedorId { get; set; }
+        public int TipoComprobanteId { get; set; }
+        public double CostoCompra { get; set; }
+        public virtual List<DetalleCompras> Detalles { get; set; }
+
+
+        public Compras()
+        {
+            CompraId = 0;
+            UsuarioId = 0;
+            FechaCompra = DateTime.Now;
+            ProveedorId = 0;
+            TipoComprobanteId = 0;
+            CostoCompra = 0;
+            Detalles = new List<DetalleCompras>();
+        }
+    }
+}
