33b6fb15fe2d344932afd2e203632ba28d848a60
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:53:27 -0400

    Creando Entidad usuario_has_permisos

---
+++ Tarea6/Entidades/Usuarios_Has_Permisos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Usuarios_Has_Permisos
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public int PermisoId { get; set; }
+
+        public Usuarios_Has_Permisos()
+        {
+            UsuarioId = 0;
+            PermisoId = 0;
+        }
+    }
+}
