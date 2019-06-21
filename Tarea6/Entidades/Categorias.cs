3d05c8e4c18fc631cbbbe65cdebf7136cd79e395
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:16:58 -0400

    creando entidad Categorias

---
+++ Tarea6/Entidades/Categorias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Categorias
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+        public string NombreCategoria { get; set; }
+        public string Descripcion { get; set; }
+
+        public Categorias()
+        {
+            CategoriaId = 0;
+            NombreCategoria = string.Empty;
+            Descripcion = string.Empty;
+        }
+    }
+}
