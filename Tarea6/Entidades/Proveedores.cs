8af0c3ca477ef74a908541738c71e410b4b6e327
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:42:00 -0400

    Creando entidad proveedores

---
+++ Tarea6/Entidades/Proveedores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Proveedores
+    {
+        [Key]
+        public int ProveedorId { get; set; }
+        public int PersonaId { get; set; }
+
+        public Proveedores()
+        {
+            ProveedorId = 0;
+            PersonaId = 0;
+        }
+    }
+}
