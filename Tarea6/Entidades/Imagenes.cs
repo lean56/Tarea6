11e0b9e8f734e4e0b8fee5d019f9ef9ba12cb551
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:57:24 -0400

    creando entidad imagenes

---
+++ Tarea6/Entidades/Imagenes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Imagenes
+    {
+        [Key]
+        public int ImagenId { get; set; }
+        public string RutaImagen { get; set; }
+
+        public Imagenes()
+        {
+            ImagenId = 0;
+            RutaImagen = string.Empty;
+        }
+    }
+}
