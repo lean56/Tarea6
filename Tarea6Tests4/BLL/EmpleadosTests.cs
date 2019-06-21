12b90e8cec192fe9bae162e5cd7a3e8b81e4c998
leonardo emil <theflaco56@hotmail.com>
2019-06-21 14:28:12 -0400

    creando estado test

--- Tarea6Tests4/BLL/EmpleadosTests.cs
+++ Tarea6Tests4/BLL/EmpleadosTests.cs
@@ -19,13 +19,53 @@ namespace Tarea6.BLL.Tests
 
             Empleados empleados = new Empleados()
             {
-                EmpleadoId = 1,
+                EmpleadoId = 2,
                 EstadoId = 1,
-                CargoId=1,
-                PersonaId=1
+                CargoId = 1,
+                PersonaId = 1
             };
 
             Assert.IsTrue(db.Guardar(empleados));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Empleados> db = new RepositorioBase<Empleados>();
+
+            Empleados empleados = new Empleados()
+            {
+                EmpleadoId = 1,
+                EstadoId = 2,
+                CargoId = 1,
+                PersonaId = 1
+            };
+
+            Assert.IsTrue(db.Modificar(empleados));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Empleados> db = new RepositorioBase<Empleados>();
+
+            Assert.IsTrue(db.Eliminar(2));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Empleados> db = new RepositorioBase<Empleados>();
+
+            Assert.IsNotNull(db.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Empleados> db = new RepositorioBase<Empleados>();
+
+            Assert.IsNotNull(db.GetList(t => true));
         }
     }
 }
---
+++ Tarea6Tests4/BLL/EstadosTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarea6.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.BLL.Tests
+{
+    [TestClass()]
+    public class EstadosTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Estados> db = new RepositorioBase<Estados>();
+
+            Estados estados = new Estados()
+            {
+                EstadoId = 1,
+                FechaInicio = DateTime.Now,
+                FechaFin = DateTime.Now,
+                Estado = "n"
+            };
+
+            Assert.IsTrue(db.Guardar(estados));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Estados> db = new RepositorioBase<Estados>();
+
+            Estados estados = new Estados()
+            {
+                EstadoId = 1,
+                FechaInicio = DateTime.Now,
+                FechaFin = DateTime.Now,
+                Estado = "nuevo"
+            };
+
+            Assert.IsTrue(db.Modificar(estados));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Estados> db = new RepositorioBase<Estados>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Estados> db = new RepositorioBase<Estados>();
+
+            Assert.IsNotNull(db.Buscar(2));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Estados> db = new RepositorioBase<Estados>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+
+   
+    }
+}
