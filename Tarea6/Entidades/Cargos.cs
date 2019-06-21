0908219ac619e309929dddfb2d090103fc93c44e
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:36:55 -0400

    creando entidad cargos

---
+++ Tarea6/Entidades/Cargos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Cargos
+    {
+        [Key]
+        public int CargoId { get; set; }
+        public int Nombre { get; set; }
+        public int EstadoId { get; set; }
+
+        public Cargos()
+        {
+            CargoId = 0;
+            Nombre = 0;
+            EstadoId = 0;
+        }
+    }
+}
