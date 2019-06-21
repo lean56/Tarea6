6164ad45db5420ac224d3572844153ac63ad1452
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:59:25 -0400

    creando entidad TipoComprobantes

---
+++ Tarea6/Entidades/TipoComprobantes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class TipoComprobantes
+    {
+        [Key]
+        public int ComprobanteId { get; set; }
+        public string NombreComprobante { get; set; }
+
+        public TipoComprobantes()
+        {
+            ComprobanteId = 0;
+            NombreComprobante = string.Empty;
+        }
+    }
+}
