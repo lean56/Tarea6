c5cdee475677c6250c1c105f8ea54f04fe98e46b
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:21:40 -0400

    creando entidad UnidadMedidas

---
+++ Tarea6/Entidades/UnidadMedidas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class UnidadMedidas
+    {
+        [Key]
+        public int UnidadMedidaId { get; set; }
+        public string NombreUnidad { get; set; }
+
+        public UnidadMedidas()
+        {
+            UnidadMedidaId = 0;
+            NombreUnidad = string.Empty;
+        }
+    }
+}
