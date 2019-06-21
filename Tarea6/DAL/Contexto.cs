a96940ccc700baa33fcd445caa0e839534517fc1
leonardo emil <theflaco56@hotmail.com>
2019-06-21 10:26:06 -0400

    agregando al contexto

--- Tarea6/DAL/Contexto.cs
+++ Tarea6/DAL/Contexto.cs
@@ -11,15 +11,11 @@ namespace Tarea6.DAL
     public class Contexto : DbContext
     {
         public DbSet<Personas>personas {get;set;}
+        public DbSet<TipoPersonas> TipoPersona { get; set; }
+        public DbSet<Clientes> Cliente { get; set; }
+        public DbSet<Cargos> Cargo { get; set; }
+        public DbSet<Categorias> Categoria { get; set; }
+        public DbSet<Compras> Compra { get; set; }
         public Contexto() : base("Constr") { }
-
-        private void FixEfProviderServicesProblem()
-        {
-            // The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
-            // for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
-            // Make sure the provider assembly is available to the running application. 
-            // See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
-            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
-        }
     }
 }
