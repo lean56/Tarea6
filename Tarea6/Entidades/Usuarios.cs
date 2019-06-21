1804398b47b7829973e8a6bf3fd610525fec5ddc
leonardo emil <theflaco56@hotmail.com>
2019-06-21 09:50:25 -0400

    creando entidad Usuarios

---
+++ Tarea6/Entidades/Usuarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+   public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public int EmpleadoId { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            EmpleadoId = 0;
+            NombreUsuario = string.Empty;
+            Clave = string.Empty;
+        }
+    }
+}
