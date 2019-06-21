2fa4dfb97b0f13217f612fcf847fd1927a217e3f
leonardo emil <theflaco56@hotmail.com>
2019-06-21 11:46:06 -0400

    cliente tests

---
+++ Tarea6Tests4/BLL/ClientesTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarea6.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.BLL.Tests
+{
+    [TestClass()]
+    public class ClientesTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Clientes clientes = new Clientes()
+            {
+                ClienteId = 2,
+                EstadoId = 1,
+                PersonaId = 1,
+                FechaCreacion = DateTime.Now
+            };
+
+            Assert.IsTrue(db.Guardar(clientes));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Clientes clientes = new Clientes()
+            {
+                ClienteId = 1,
+                EstadoId = 2,
+                PersonaId = 2,
+                FechaCreacion = DateTime.Now
+            };
+
+            Assert.IsTrue(db.Modificar(clientes));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsNotNull(db.Buscar(2));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+    }
+}
---
+++ Tarea6Tests4/BLL/ComprasTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarea6.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea6.Entidades;
+
+namespace Tarea6.BLL.Tests
+{
+    [TestClass()]
+    public class ComprasTests
+    {
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Compras> db = new RepositorioBase<Compras>();
+            List<DetalleCompras> lista = new List<DetalleCompras>();
+            lista.Add(new DetalleCompras()
+            {
+                CompraId = 1,
+                DetalleCompraId = 1,
+                Unidades = 50,
+                Total = 50,
+                CostoUnidad = 500,
+                ProductoId = 1
+            });
+            lista.Add(new DetalleCompras()
+            {
+                CompraId = 1,
+                DetalleCompraId = 1,
+                Unidades = 100,
+                Total = 140,
+                CostoUnidad = 390,
+                ProductoId = 1
+            });
+        }
+    }
+}
